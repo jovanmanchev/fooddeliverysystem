3551bffa5594ff253b383a0c97228ae03b6262d4
jovanmanchev <jovanmanchev3003@gmail.com>
2022-12-28 19:34:06 +0100

    auth provider added

--- fooddeliverysystem/src/main/java/com/example/fooddeliverysystem/configuration/WebSecurityConfiguration.java
+++ fooddeliverysystem/src/main/java/com/example/fooddeliverysystem/configuration/WebSecurityConfiguration.java
@@ -4,12 +4,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfiguration   {
+
+    private final PasswordEncoder passwordEncoder;
+    private final UsernameAndPasswordAuthProvider usernameAndPasswordAuthProvider;
+
+    public WebSecurityConfiguration(PasswordEncoder passwordEncoder, UsernameAndPasswordAuthProvider usernameAndPasswordAuthProvider) {
+        this.passwordEncoder = passwordEncoder;
+        this.usernameAndPasswordAuthProvider = usernameAndPasswordAuthProvider;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
