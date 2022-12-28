609cb9b90b634f696fce8d734f0f9497809b81cd
jovanmanchev <jovanmanchev3003@gmail.com>
2022-12-28 19:21:11 +0100

    security initialized

--- fooddeliverysystem/src/main/java/com/example/fooddeliverysystem/service/impl/UserServiceImplementation.java
+++ fooddeliverysystem/src/main/java/com/example/fooddeliverysystem/service/impl/UserServiceImplementation.java
@@ -2,19 +2,21 @@ package com.example.fooddeliverysystem.service.impl;
 
 import com.example.fooddeliverysystem.exceptions.UserNotFoundException;
 import com.example.fooddeliverysystem.model.User;
-//import com.example.fooddeliverysystem.model.UserPrincipal;
+
 import com.example.fooddeliverysystem.model.enums.Role;
+import com.example.fooddeliverysystem.model.userssecurity.UserPrincipal;
 import com.example.fooddeliverysystem.repository.*;
 import com.example.fooddeliverysystem.service.UserService;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.config.authentication.UserServiceBeanDefinitionParser;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class UserServiceImplementation implements UserService {
+public class UserServiceImplementation implements UserService, UserDetailsService {
     private final UserRepository userRepository;
     private final AdminRepository adminRepository;
     private final ConsumerRepository consumerRepository;
@@ -37,20 +39,20 @@ public class UserServiceImplementation implements UserService {
 
 
 
-//    @Override
-//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//        User user = this.userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Username not found"));
-//        Long id = user.getUser_id();
-//        if(this.adminRepository.findById(id).isPresent())
-//            user.setUserRole(Role.ADMIN);
-//        else if(this.consumerRepository.findById(id).isPresent())
-//            user.setUserRole(Role.CONSUMER);
-//        else if(this.deliverRepository.findById(id).isPresent())
-//            user.setUserRole(Role.DELIVER);
-//        else
-//            user.setUserRole(Role.SALEPLACEEMPLOYEE);
-//        return new UserPrincipal(user);
-//    }
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+        Long id = user.getUser_id();
+        if(this.adminRepository.findById(id).isPresent())
+            user.setUserRole(Role.ADMIN);
+        else if(this.consumerRepository.findById(id).isPresent())
+            user.setUserRole(Role.CONSUMER);
+        else if(this.deliverRepository.findById(id).isPresent())
+            user.setUserRole(Role.DELIVER);
+        else
+            user.setUserRole(Role.SALEPLACEEMPLOYEE);
+        return new UserPrincipal(user);
+    }
 
 
 }
