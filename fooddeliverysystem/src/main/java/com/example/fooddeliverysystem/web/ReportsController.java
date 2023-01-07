76291bdb29e8a519a52c512d3eacf9feffd2c0ff
jovanmanchev <jovanmanchev3003@gmail.com>
2023-01-07 21:48:22 +0100

    added most loyal customer for each place view

--- fooddeliverysystem/src/main/java/com/example/fooddeliverysystem/web/ReportsController.java
+++ fooddeliverysystem/src/main/java/com/example/fooddeliverysystem/web/ReportsController.java
@@ -1,6 +1,7 @@
 package com.example.fooddeliverysystem.web;
 
 import com.example.fooddeliverysystem.service.BestPacketsForEachSalePlaceService;
+import com.example.fooddeliverysystem.service.MostLoyalCustomerForEachPlaceService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,14 +10,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ReportsController {
 
     private final BestPacketsForEachSalePlaceService bestPacketsForEachSalePlaceService;
-
-    public ReportsController(BestPacketsForEachSalePlaceService bestPacketsForEachSalePlaceService) {
+    private final MostLoyalCustomerForEachPlaceService mostLoyalCustomerForEachPlaceService;
+    public ReportsController(BestPacketsForEachSalePlaceService bestPacketsForEachSalePlaceService, MostLoyalCustomerForEachPlaceService mostLoyalCustomerForEachPlaceService) {
         this.bestPacketsForEachSalePlaceService = bestPacketsForEachSalePlaceService;
+        this.mostLoyalCustomerForEachPlaceService = mostLoyalCustomerForEachPlaceService;
     }
 
     @GetMapping("/reports/bestPacketsEachSalePlace")
     public String getBestPacketsEachSalePlace(Model model){
         model.addAttribute("report", this.bestPacketsForEachSalePlaceService.listAll());
         return "reportsBestPacketsEachSalePlace";
+    }
+    @GetMapping("/reports/mostLoyalCustomerForEachPlace")
+    public String getMostLoyalCustomerForEachPlace(Model model){
+        model.addAttribute("report", this.mostLoyalCustomerForEachPlaceService.listAll());
+        return "reportsMostLoyalCustomerForEachPlace";
     }
 }
